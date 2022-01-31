8845396003f620ca20b4d597f72817eeec8dc5fa
manjugoutham <gouthamy219@gmail.com>
2022-01-31 08:33:41 +0530

    To set Event Listeners when Document is loaded

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -75,3 +75,31 @@ class EmployeePayrollData{
        }
 }
 
+/**
+ * Ability to set Event Listeners when Document is loaded so as to.
+ */
+//..........UC2...........
+window.addEventListener('DOMContentLoaded',(event) => {
+    const name = document.querySelector('#name');
+    const textError =  document.querySelector('.text-error');
+    name.addEventListener('input',function(){
+    if(name.value.length==0){
+        textError.textContent = "";
+        return;
+    }
+    try {
+       (new EmployeePayrollData()).name = name.value;;
+       textError.textContent = ""; 
+    } catch (e) {
+        textError.textContent = e;
+    }  
+    });
+    
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function(){
+        output.textContent = salary.value;
+      
+    });
+});
